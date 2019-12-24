130de16afd3557969e2ad77768e2acd67e56c2b2
zeng <xiaolu>
2019-12-24 10:51:00 +0800

    basic study

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_study/study01/basic01.dart';
 import 'package:flutter_study/study01/basic02.dart';
 import 'package:flutter_study/study01/basic03.dart';
+import 'package:flutter_study/study01/basic04.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -10,12 +12,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //basic01
+      //basic01  一个简单的Scaffold自定义
 //      home: MyScaffold(),
-      //basic02
-//      home: TutorialHome(),
-      //basic03
-      home: MyButton(),
+      //basic02  Scaffold的基本使用
+//      home: TutorialHome(),R®
+      //basic03  处理基本的手势操作
+//      home: MyButton(),
+      //basic 04 处理基本的状态改变
+      home: Counter(),
     );
   }
 }
---
+++ lib/study01/basic04.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+//处理基本的状态改变
+class Counter extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CounterState();
+}
+
+class _CounterState extends State {
+  int _count = 0;
+
+  @override
+  void initState() {
+    print("initState");
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    print("dispose");
+    super.dispose();
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    print("setState");
+    super.setState(fn);
+  }
+
+  void _change() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        RaisedButton(
+          onPressed: _change,
+          child: Text("Count:$_count"),
+        )
+      ],
+    );
+  }
+}
